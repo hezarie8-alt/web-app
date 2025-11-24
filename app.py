5a9b16936703d7b6869717e73d74fad9b74023ad
hezarie8-alt <hezarie8@gmail.com>
2025-11-24 02:32:42 -0800

    new update

--- app.py
+++ app.py
@@ -91,7 +91,6 @@ class User(db.Model):
     name = db.Column(db.String(100), nullable=False, unique=True)
     major = db.Column(db.String(100))
     password_hash = db.Column(db.String(128), nullable=False)
-    created_at = db.Column(db.DateTime, server_default=db.func.now())  # اضافه شده
 
 class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
